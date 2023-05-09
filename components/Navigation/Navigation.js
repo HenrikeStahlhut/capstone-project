08bf8109fa8e557ee3186ef09eb33f11df0232c9
Henrike Stahlhut <henrike.st@icloud.com>
2023-05-09 15:47:58 +0200

    add minimal navigation styling

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,35 +1,61 @@
 import styled from "styled-components";
+import Image from "next/image";
+import { IoAddCircleSharp } from "react-icons/io5";
 
 // Styled components
 
-const NavContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  max-width: 20em;
-  background-color: pink;
-  border-radius: 20px;
-  margin: 30px auto;
+const NavContainer = styled.nav`
+  position: fixed;
+  width: 15em;
+  background-color: #c2d9b4;
+  border-radius: 30px;
+  bottom: 2em;
+  left: 5em;
 `;
 
 const Nav = styled.ul`
   list-style: none;
   padding: 0;
   display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const ListItemContainer = styled.div`
+  display: flex;
   flex-direction: column;
+  margin: 0px 10px;
 `;
 
 const ListItem = styled.li`
-  color: black;
+  cursor: pointer;
+  padding-top: 0.5em;
+  font-size: 10px;
+  text-align: center;
 `;
 
-const ListIcon = styled.svg``;
+// const ListIcon = styled.svg`
+//   height: 30px;
+//   width: 30px;
+// `;
 
 export default function Navigation() {
   return (
     <>
       <NavContainer>
         <Nav>
-          <ListItem>Add</ListItem>
+          <ListItemContainer>
+            <IoAddCircleSharp size="30px" />
+            <ListItem>Home</ListItem>
+          </ListItemContainer>
+          <ListItemContainer>
+            <IoAddCircleSharp size="30px" />
+            <ListItem>Add</ListItem>
+          </ListItemContainer>
+          <ListItemContainer>
+            <IoAddCircleSharp size="30px" />
+            <ListItem>To-Do</ListItem>
+          </ListItemContainer>
         </Nav>
       </NavContainer>
     </>
