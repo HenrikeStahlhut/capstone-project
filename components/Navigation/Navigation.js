8c7ff374dd89d80e41e54854ca2febe3fc8ccaa4
Henrike Stahlhut <henrike.st@icloud.com>
2023-05-09 14:29:51 +0200

    add navigation and minimal styling

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,3 +1,37 @@
+import styled from "styled-components";
+
+// Styled components
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 20em;
+  background-color: pink;
+  border-radius: 20px;
+  margin: 30px auto;
+`;
+
+const Nav = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListItem = styled.li`
+  color: black;
+`;
+
+const ListIcon = styled.svg``;
+
 export default function Navigation() {
-  return <></>;
+  return (
+    <>
+      <NavContainer>
+        <Nav>
+          <ListItem>Add</ListItem>
+        </Nav>
+      </NavContainer>
+    </>
+  );
 }
