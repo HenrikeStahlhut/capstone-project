f8af7b38d8d7d6273b3420479de67407f6013ab5
Henrike Stahlhut <henrike.st@icloud.com>
2023-05-11 12:35:54 +0200

    relocate styled components in RoomTile Component

---
+++ components/RoomTile/RoomTile.Styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledTileTitle = styled.p`
+  font-size: 15px;
+  margin: 10px 0px 0px 0px;
+`;
+
+export const StyledImageContainer = styled.div`
+  overflow: hidden;
+  border-radius: 20px;
+  width: 128px;
+  height: 128px;
+`;
--- components/RoomTile/RoomTile.js
+++ components/RoomTile/RoomTile.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styled from "styled-components";
+import { StyledTileTitle, StyledImageContainer } from "./RoomTile.Styled";
 
 // Freezed Object which contains all types of rooms (e.g. kitchen) that the user can choose from later on when adding a room
 export const RoomType = Object.freeze({
@@ -13,31 +13,18 @@ export const RoomType = Object.freeze({
   OFFICE: "office",
 });
 
-// Styled Component
-const Title = styled.p`
-  font-size: 15px;
-  margin: 10px 0px 0px 0px;
-`;
-
-const ImageContainer = styled.div`
-  overflow: hidden;
-  border-radius: 20px;
-  width: 128px;
-  height: 128px;
-`;
-
 export default function RoomTile({ title, type }) {
   return (
     <div>
-      <ImageContainer>
+      <StyledImageContainer>
         <Image
           src={`/images/rooms/${type}.png`}
           width={128}
           height={128}
           alt={title}
         />
-      </ImageContainer>
-      <Title>{title}</Title>
+      </StyledImageContainer>
+      <StyledTileTitle>{title}</StyledTileTitle>
     </div>
   );
 }
