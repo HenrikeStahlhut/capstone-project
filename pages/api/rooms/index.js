3c39f794300f61e3a244f69adca89aaa2810eafd
Henrike <126799897+HenrikeStahlhut@users.noreply.github.com>
2023-05-23 09:48:01 +0200

    Revert "Edit room"

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
     const { title, type, rooms } = req.body;
 
     const room = await createRoom({ title, type, rooms });
+    // const room = await createRoom({ title, type });
 
     return res.status(201).json(room);
   }
