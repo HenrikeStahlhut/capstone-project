b27e922f2baf86859f92e457a1de4639a508cfdb
Henrike Stahlhut <henrike.st@icloud.com>
2023-06-01 11:15:19 +0200

    add react-loading

---
+++ components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledLoadingContainer = styled.div`
+  width: 100px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+`;
+
+export default function Loading() {
+  return <div>Loading...</div>;
+}
